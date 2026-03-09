a1544d6250d31e074fac3702a68723dd99487cb2
tedenes <denestemleitner93@gmail.com>
2026-03-10 00:30:19 +0100

    Refactor home page buttons to use anchor tags and update text to Hungarian

--- src/app/components/menu/menu.spec.ts
+++ src/app/components/menu/menu.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { Menu } from './menu';
 
 describe('Menu', () => {
@@ -8,7 +8,8 @@ describe('Menu', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Menu]
+      imports: [Menu],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
